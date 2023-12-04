f2b2e439b8898575ae054fb5c382f3cc41037175
Janko Jovičić <jankojovicic351@gmail.com>
2023-12-04 15:11:25 +0100

    Working deap solution

--- deap_solver_dependency.py
+++ deap_solver_dependency.py
@@ -14,21 +14,16 @@ class ProductionJob:
         self.start_time = None
         self.end_time = None
 
-
 class TimeSlot:
     def __init__(self, start, end):
         self.start = start
         self.end = end
 
-
 class ProductionCalendar:
     def __init__(self, work_periods):
         self.work_periods = [TimeSlot(start, end) for start, end in work_periods]
 
-
 def evaluate(individual, jobs):
-    valid_jobs = [job for job in jobs if job.end_time is not None and job.deadline is not None]
-
     # Calculate the total duration of the schedule
     total_duration = sum(individual)
 
@@ -38,8 +33,10 @@ def evaluate(individual, jobs):
     # Calculate the total resource utilization
     total_utilization = sum([job.actual_duration for job, duration in zip(jobs, individual)])
 
-    # Calculate the total deviation from deadlines for jobs with valid deadlines and end times
-    total_deadline_deviation = sum([max(0, job.end_time - job.deadline) for job in valid_jobs])
+    # Calculate the total deviation from deadlines
+    total_deadline_deviation = sum(
+        [max(0, job.end_time - job.deadline if job.end_time is not None and job.deadline is not None else 0) for job in
+         jobs if job.deadline is not None])
 
     # Return a tuple of the multiple objectives
     return total_duration, -idle_time, total_utilization, total_deadline_deviation
@@ -54,7 +51,6 @@ def dynamic_priority(job, current_time):
     else:
         return 0
 
-
 def create_production_schedule(jobs, company_calendar, resource_calendars):
     # Particle Swarm Optimization parameters
     POPULATION_SIZE = 10
@@ -62,30 +58,19 @@ def create_production_schedule(jobs, company_calendar, resource_calendars):
     ITERATIONS = 10
 
     # Create a creator for the individual (schedule)
-    creator.create("FitnessMulti", base.Fitness,
-                   weights=(1.0, -1.0, -1.0, -1.0))  # Minimize duration, idle time, utilization, and deadline deviation
+    creator.create("FitnessMulti", base.Fitness, weights=(1.0, -1.0, -1.0, -1.0))  # Minimize duration, idle time, utilization, and deadline deviation
     creator.create("Particle", list, fitness=creator.FitnessMulti, speed=list, pmin=None, pmax=None, best=None)
 
     # Create a toolbox for the PSO algorithm
     toolbox = base.Toolbox()
     toolbox.register("particle", tools.initRepeat, creator.Particle,
-                     lambda: random.uniform(0, max(job.max_duration for job in jobs)), n=PARTICLE_SIZE)
+                     lambda: random.uniform(0, max([job.max_duration for job in jobs])), n=PARTICLE_SIZE)
     toolbox.register("population", tools.initRepeat, list, toolbox.particle)
     toolbox.register("mate", tools.cxBlend, alpha=0.5)
     toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
     toolbox.register("select", tools.selBest)
     toolbox.register("evaluate", evaluate, jobs=jobs)
 
-    current_time = 0
-    jobs.sort(key=lambda job: dynamic_priority(job, current_time), reverse=True)
-
-    # Sort jobs based on level (lower level first)
-    jobs.sort(key=lambda job: job.level)
-
-    # Group jobs by level
-    grouped_jobs = {}
-    for job in jobs:
-        grouped_jobs.setdefault(job.level, []).append(job)
     # Initialize the particle population
     population = toolbox.population(n=POPULATION_SIZE)
 
@@ -129,54 +114,43 @@ def create_production_schedule(jobs, company_calendar, resource_calendars):
     best_particle = tools.selBest(population, k=1)[0]
 
     # Update job start and end times based on the best individual
+    # Update job start and end times based on the best individual
+    # Find jobs without dependencies
+    independent_jobs = [job for job in jobs if not job.dependencies]
+
+    # Find jobs with dependencies
+    jobs_with_dependencies = [job for job in jobs if job.dependencies]
+
+    # Sort jobs with dependencies based on priority
+    jobs_with_dependencies.sort(key=lambda job: dynamic_priority(job, 0), reverse=True)
+
+    # Initialize start times for jobs without dependencies
     current_time = 0
-    for level, level_jobs in grouped_jobs.items():
-        # Filter jobs on the same level that don't use the same resource and have no dependencies
-        same_time_jobs = []
-        used_resources = set()
-        for job in level_jobs:
-            resource_key = frozenset(job.resource_requirements.items())  # Convert dict to frozenset for hashing
-            if job.dependencies or resource_key in used_resources:
-                same_time_jobs.append(job)  # If job has dependencies or uses the same resource, add it to the list
-            else:
-                same_time_jobs.append(job)
-                used_resources.add(resource_key)
+    for job in independent_jobs:
+        job.start_time = current_time
+        job.actual_duration = (job.min_duration + job.max_duration) / 2
+        job.end_time = job.start_time + job.actual_duration
 
-        # Update start and end times for jobs that can run simultaneously
-        max_start_time = 0  # Track the maximum start time for jobs on the same level
-        for job in same_time_jobs:
-            if not job.dependencies:
-                job.actual_duration = (job.min_duration + job.max_duration) / 2
-                job.start_time = current_time if job.start_time is None else job.start_time
-                job.end_time = job.start_time + job.actual_duration
-                max_start_time = max(max_start_time, job.end_time)  # Update the maximum start time
+    # Map job IDs to their respective instances for easy access
+    job_map = {job.id: job for job in jobs}
 
-        # Update current_time based on the maximum start time of jobs on the same level
-        current_time = max(max_start_time, current_time)
+    # Schedule jobs with dependencies considering their start times
+    for job in jobs_with_dependencies:
+        dependencies = job.dependencies
+        max_end_time = max(
+            job_map[dependency].end_time for dependency in dependencies if job_map[dependency].end_time is not None)
+        job.start_time = max(max_end_time if max_end_time is not None else 0,
+                             job.start_time if job.start_time is not None else 0)
+        job.actual_duration = (job.min_duration + job.max_duration) / 2
+        job.end_time = job.start_time + job.actual_duration
+
+    # ... existing code ...
 
     return jobs
 
-
-class UncertainProductionJob(ProductionJob):
-    def __init__(self, id, name, level, min_duration, max_duration, resource_requirements, dependencies=None,
-                 deadline=None, time_window=None, alternative_resources=None):
-        super().__init__(id, name, 0, resource_requirements, dependencies, deadline)
-        self.min_duration = min_duration
-        self.max_duration = max_duration
-        self.actual_duration = 0
-        self.time_window = time_window  # Time window during which the job can be scheduled
-        self.alternative_resources = alternative_resources if alternative_resources else []
-        self.level = level
-
-
-
-
-
-
 # Example usage
 if __name__ == "__main__":
-    company_calendar = ProductionCalendar(
-        [(8, 12), (13, 17)])  # Company works from 8 am to 5 pm with a break from 12 pm to 1 pm
+    company_calendar = ProductionCalendar([(8, 12), (13, 17)])  # Company works from 8 am to 5 pm with a break from 12 pm to 1 pm
 
     resource_calendars = {
         "resource1": ProductionCalendar([(8, 12), (13, 17)]),
@@ -184,11 +158,19 @@ if __name__ == "__main__":
         "resource3": ProductionCalendar([(8, 12), (13, 16)]),
     }
 
-    job1 = UncertainProductionJob(1, "Job1", 2, 4, 6, {"resource1": 1}, time_window=(8, 12), deadline=15)
-    job2 = UncertainProductionJob(2, "Job2", 1, 2, 4, {"resource1": 1}, deadline=20,
-                                  alternative_resources=["resource2"])
-    job3 = UncertainProductionJob(3, "Job3", 1, 3, 5, {"resource1": 1}, time_window=(13, 16))
-    job4 = UncertainProductionJob(4, "Job4", 2, 1, 3, {"resource1": 1}, )
+    class UncertainProductionJob(ProductionJob):
+        def __init__(self, id, name, min_duration, max_duration, resource_requirements, dependencies=None, deadline=None, time_window=None, alternative_resources=None):
+            super().__init__(id, name, 0, resource_requirements, dependencies, deadline)
+            self.min_duration = min_duration
+            self.max_duration = max_duration
+            self.actual_duration = 0
+            self.time_window = time_window  # Time window during which the job can be scheduled
+            self.alternative_resources = alternative_resources if alternative_resources else []
+
+    job1 = UncertainProductionJob(1, "Job1", 4, 6, {"resource1": 4})
+    job2 = UncertainProductionJob(2, "Job2", 2, 4, {"resource2": 2}, dependencies=[1])
+    job3 = UncertainProductionJob(3, "Job3", 3, 5, {"resource3": 1}, dependencies=[1, 2])
+    job4 = UncertainProductionJob(5, "Job5", 1, 3, {"resource3": 1})
 
     jobs = [job1, job2, job3, job4]
 
@@ -200,5 +182,4 @@ if __name__ == "__main__":
 
     if scheduled_jobs:
         for job in scheduled_jobs:
-            print(
-                f"Job {job.name} scheduled from {job.start_time} to {job.end_time} with resources: {job.resource_requirements}, actual duration: {job.actual_duration}, time window: {job.time_window}, alternative resources: {job.alternative_resources}, deadline: {job.deadline}")
+            print(f"Job {job.name} scheduled from {job.start_time} to {job.end_time} with resources: {job.resource_requirements}, actual duration: {job.actual_duration}, time window: {job.time_window}, alternative resources: {job.alternative_resources}, deadline: {job.deadline}")
